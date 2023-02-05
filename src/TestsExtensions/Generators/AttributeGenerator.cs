d4b85e4e4cc69511040532faf588db4eadfeac2e
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-02-05 17:08:08 +0100

    replace guid with int

--- src/TestsExtensions/Generators/AttributeGenerator.cs
+++ src/TestsExtensions/Generators/AttributeGenerator.cs
@@ -17,11 +17,14 @@ internal class AttributeGenerator : ISourceGenerator
             typeof(JsonDataAttribute),
             context.CancellationToken);
 
+        var i = 1;
         foreach (var group in methodGroups)
         {
             var source = GenerateAttribute(group);
 
-            context.AddSource($"SignatureWrapper.{group.Key}.{Guid.NewGuid()}.g.cs", SourceText.From(source, Encoding.UTF8));
+            var fileNumber = i.ToString().PadLeft(6, '0');
+            context.AddSource($"SignatureWrapper.{group.Key}.{fileNumber}.g.cs", SourceText.From(source, Encoding.UTF8));
+            i++;
         }
     }
 
