83c13a7689ddc1ab23910caeee22a7b214f7be8d
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-02-05 16:32:03 +0100

    temp

--- examples/TestsExtensions.Examples.xUnit/Generated/SignatureWrapper_01.cs
+++ examples/TestsExtensions.Examples.xUnit/Generated/SignatureWrapper_01.cs
@@ -1,23 +1,23 @@
-using System.Text.Json;
-
-namespace TestsExtensions.Examples.xUnit.Generated;
-
-// Example of generated class for Calculator_Add test
-// ReSharper disable once UnusedType.Local
-file record SignatureWrapper : ISignatureWrapper
-{
-    public string Key => "CalculatorExample/TestData/Calculator_Add.json";
-    public int InputA { get; init; } = default!;
-    public int InputB { get; init; } = default!;
-    public int Expected { get; init; } = default!;
-
-    public IEnumerable<object[]> Deserialize(string json)
-    {
-        var datalist = JsonSerializer.Deserialize<List<SignatureWrapper>>(json);
-        if (datalist is null) return new List<object[]>();
-
-        return datalist
-            .Select(data => new object[] { data.InputA, data.InputB, data.Expected }) //order matters
-            .ToList();
-    }
-}
+// using System.Text.Json;
+//
+// namespace TestsExtensions.Examples.xUnit.Generated;
+//
+// // Example of generated class for Calculator_Add test
+// // ReSharper disable once UnusedType.Local
+// file record SignatureWrapper : ISignatureWrapper
+// {
+//     public string Key => "CalculatorExample/TestData/Calculator_Add.json";
+//     public int InputA { get; init; } = default!;
+//     public int InputB { get; init; } = default!;
+//     public int Expected { get; init; } = default!;
+//
+//     public IEnumerable<object[]> Deserialize(string json)
+//     {
+//         var datalist = JsonSerializer.Deserialize<List<SignatureWrapper>>(json);
+//         if (datalist is null) return new List<object[]>();
+//
+//         return datalist
+//             .Select(data => new object[] { data.InputA, data.InputB, data.Expected }) //order matters
+//             .ToList();
+//     }
+// }
