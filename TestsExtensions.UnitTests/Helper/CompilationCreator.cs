f8e0b9c7bb1408b7be870e61c5c707e28a2f4ccc
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-02-07 00:02:30 +0100

    add TestExtensions.UnitTests project add Compilation Creator add SignatureWrapperGeneratorTests

---
+++ TestsExtensions.UnitTests/Helper/CompilationCreator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+
+namespace TestsExtensions.UnitTests.Helper
+{
+    internal static class CompilationCreator
+    {
+        internal static Compilation CreateCompilation(string sourceCode)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+            var references = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .Where(assembly => !assembly.IsDynamic)
+                .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
+                .Cast<MetadataReference>();
+
+            var compilation = CSharpCompilation.Create(
+                "SourceGeneratorTests",
+                new[] { syntaxTree },
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            return compilation;
+        }
+    }
+}
