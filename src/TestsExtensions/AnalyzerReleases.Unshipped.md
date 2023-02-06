45d49a0de2b2e9f91509c8ecb8e71a1640c46a03
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-02-06 17:35:04 +0100

    add descriptors class

---
+++ src/TestsExtensions/AnalyzerReleases.Unshipped.md
@@ -0,0 +1,8 @@
+﻿; Unshipped analyzer release
+; https://github.com/dotnet/roslyn-analyzers/blob/main/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+TE0001 | TestsExtensions analyzer | Error | TextExtensionsDescriptors
---
+++ src/TestsExtensions/Analyzers/TextExtensionsDescriptors.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace TestsExtensions.Analyzers
+{
+    public class TextExtensionsDescriptors
+    {
+        public static readonly DiagnosticDescriptor JsonDataAttributeMustHaveUniquePathParam
+            = new(
+                "TE0001",
+                "The JsonDataAttribute must have a unique path parameter",
+                "The duplicated JsonDataAttribute has the same path. Change the path in one of the attributes.",
+                "TestsExtensions analyzer",
+                DiagnosticSeverity.Error,
+                true);
+    }
+}
