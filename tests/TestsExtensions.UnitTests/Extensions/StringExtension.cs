8a67df9e95a1b9dc0c7151fe86679b28938404e4
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-02-10 17:22:41 +0100

    add unit test for SignatureWrapperBuilder

---
+++ tests/TestsExtensions.UnitTests/Extensions/StringExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace TestsExtensions.UnitTests.Extensions;
+
+internal static class StringExtension
+{
+    internal static string RemoveWhitespace(this string input)
+        => new(input.ToCharArray()
+            .Where(c => !Char.IsWhiteSpace(c))
+            .ToArray());
+}
---
+++ tests/TestsExtensions.UnitTests/Helper/SyntaxNodeCreator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace TestsExtensions.UnitTests.Helper;
+
+internal static class SyntaxNodeCreator
+{
+    internal static IEnumerable<T> GetSyntaxNodes<T>(string context)
+        where T : SyntaxNode
+        => CSharpSyntaxTree
+        .ParseText(context)
+        .GetRoot()
+        .DescendantNodes()
+        .OfType<T>();
+
+
+}
